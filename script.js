57ec2067d3fed343c5fbaa7c69f0d7342410d0e4
ManfredNelvin <119521670+ManfredNelvin@users.noreply.github.com>
2023-07-11 22:15:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+  
+  get species() {
+    return this._species;
+  }
+  
+  makeSound() {
+    // Default implementation
+  }
+}
 
-class Dog extends Animal {}
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+  
+  makeSound() {
+    this.purr();
+  }
+}
 
-class Cat extends Animal {}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+  
+  makeSound() {
+    this.bark();
+  }
+}
